3d6f9d050718fe648b99e15c33278bb7c9dbfb8f
a.kiperku <a.kiperku@infomaximum.com>
2023-08-08 16:15:17 +0300

    login

---
+++ dist/tools/update/hwid.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE IF NOT EXISTS `hwid_log` (
+  `server_id` INT(10) NOT NULL,
+  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `account` VARCHAR(16) NOT NULL,
+  `ip` VARCHAR(16) NOT NULL,
+  `hwid` VARCHAR(32) NOT NULL,
+  PRIMARY KEY  (`server_id`,`time`,`account`),
+  KEY `account` (`account`),
+  KEY `ip` (`ip`),
+  KEY `hwid` (`hwid`)
+) ENGINE=MyISAM;
